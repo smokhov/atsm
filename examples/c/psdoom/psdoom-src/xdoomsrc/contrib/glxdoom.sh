f00b0a08013bf6543da1353e42c21789ba3899f8
Serguei Mokhov <mokhov@cse.concordia.ca>
2015-10-12 20:25:59 -0400

    Add base psdoom from psdoom.sf.net.

---
+++ examples/c/psdoom/psdoom-src/xdoomsrc/contrib/glxdoom.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#
+# If you modified LDIR in the top level makefile, you need to modify
+# it here to the same directory too.
+#
+
+# *** PID BEGIN ***
+# Changed this to use a psdoom directory rather than xdoom.
+LDIR=/usr/local/games/psdoom
+# old code:
+#LDIR=/usr/local/games/xdoom
+# *** PID END ***
+
+[ "$DOOMWADDIR" ] || DOOMWADDIR=${LDIR}
+PATH=${LDIR}:${PATH}
+export DOOMWADDIR PATH
+
+# *** PID BEGIN ***
+exec ${LDIR}/ps-glxdoom $*
+# old code:
+#exec ${LDIR}/glxdoom $*
+# *** PID END ***
+
---
+++ examples/c/psdoom/psdoom-src/xdoomsrc/contrib/xdoom.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#
+# If you modified LDIR in the top level makefile, you need to modify
+# it here to the same directory too.
+#
+
+# *** PID BEGIN ***
+# Changed this to use a psdoom directory rather than xdoom.
+LDIR=/usr/local/games/psdoom
+# old code:
+#LDIR=/usr/local/games/xdoom
+# *** PID END ***
+
+[ "$DOOMWADDIR" ] || DOOMWADDIR=${LDIR}
+PATH=${LDIR}:${PATH}
+export DOOMWADDIR PATH
+
+# *** PID BEGIN ***
+exec ${LDIR}/ps-xdoom $*
+# old code:
+#exec ${LDIR}/xdoom $*
+# *** PID END ***
