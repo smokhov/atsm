7faeef26ff9db3398aa7decb9ffd4799a810854a
haotao.lai <haotao.lai@gmail.com>
2018-01-31 13:47:27 -0500

    fix nameing in the soap store example

--- examples/ws/soap/StoreExample/StoreServer/src/MyStoreServer.java
+++ examples/ws/soap/StoreExample/StoreServer/src/MyStoreServer.java
@@ -2,41 +2,48 @@ import java.util.HashMap;
 
 public class MyStoreServer {
 	
-	HashMap<String, Integer> inventory = new HashMap<>();
+	HashMap<String, Integer> oInventory = new HashMap<>();
 	
-	public MyStoreServer() {
-		this.inventory.put("CoffeeBag", 20);
-		this.inventory.put("Apple", 100);
-		this.inventory.put("Banana", 50);
+	public MyStoreServer()
+	{
+		this.oInventory.put("CoffeeBag", 20);
+		this.oInventory.put("Apple", 100);
+		this.oInventory.put("Banana", 50);
 		this.displayContent();
 	}
 
-	public void displayContent() {
-		System.out.println("----------------Store Inventory -----------------");
-		System.out.println("1. CofeeBags: +" + this.inventory.get("CoffeeBag"));
-		System.out.println("2. Apples: +" + this.inventory.get("Apple"));
-		System.out.println("3. Bananas: +" + this.inventory.get("Banana"));
+	public void displayContent()
+	{
+		System.out.println("----------------Store inventory -----------------");
+		System.out.println("1. CofeeBags: +" + this.oInventory.get("CoffeeBag"));
+		System.out.println("2. Apples: +" + this.oInventory.get("Apple"));
+		System.out.println("3. Bananas: +" + this.oInventory.get("Banana"));
 	}
 
-	public String buyItems(String item, int quantity) {
-		if (this.inventory.containsKey(item)) {
-			int initialQuantity = this.inventory.get(item);
-			int newQuantity;
-			if (initialQuantity - quantity >= 0) {
-				newQuantity = initialQuantity - quantity;
-				this.inventory.remove(item);
-				this.inventory.put(item,  newQuantity);
-				return quantity + " " + item + " were successfully purchased!\n"
-						+ ("1. CofeeBags: " + this.inventory.get("CoffeeBag") + "\n")
-						+ ("2. Apples: " + this.inventory.get("Apple") + "\n")
-						+ ("3. Bananas: " + this.inventory.get("Banana") + "\n")
+	public String buyItems(String strItem, int iQuantity)
+	{
+		if (this.oInventory.containsKey(strItem))
+		{
+			int _iInitialQuantity = this.oInventory.get(strItem);
+			int _iNewQuantity;
+			if (_iInitialQuantity - iQuantity >= 0)
+			{
+				_iNewQuantity = _iInitialQuantity - iQuantity;
+				this.oInventory.remove(strItem);
+				this.oInventory.put(strItem,  _iNewQuantity);
+				return iQuantity + " " + strItem + " were successfully purchased!\n"
+						+ ("1. CofeeBags: " + this.oInventory.get("CoffeeBag") + "\n")
+						+ ("2. Apples: " + this.oInventory.get("Apple") + "\n")
+						+ ("3. Bananas: " + this.oInventory.get("Banana") + "\n")
 						+ ("--------------------------------------------\n");
 			}
-			else {
+			else
+			{
 				return "There was an error with your purchase";
 			}
 		}
-		else {
+		else 
+		{
 			return "There was an error with your purchase";
 		}
 	}
